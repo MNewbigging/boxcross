7b2bae21ab458b896bc731a08eb9c89e3ba81b2e
MNewbigging <mrnewbigging@gmail.com>
2023-08-11 14:19:08 +0100

    basic player movement

---
+++ src/game/game-state.ts
@@ -0,0 +1,105 @@
+import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+import { GameLoader } from "../loaders/game-loader";
+import { KeyboardListener } from "../listeners/keyboard-listener";
+import { Renderer } from "./renderer";
+import { buildLane } from "../utils/world-builder";
+
+export class GameState {
+  private keyboardListener = new KeyboardListener();
+
+  private scene = new THREE.Scene();
+  private camera: THREE.PerspectiveCamera;
+  private renderer: Renderer;
+  private controls: OrbitControls;
+  private clock = new THREE.Clock();
+
+  private player?: THREE.Object3D;
+  private playerMoveSpeed = 5;
+
+  constructor(
+    private canvas: HTMLCanvasElement,
+    private gameLoader: GameLoader
+  ) {
+    // Setup camera
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      canvas.clientWidth / canvas.clientHeight,
+      0.1,
+      1000
+    );
+    this.camera.position.set(25, 30, 0);
+
+    // Setup renderer
+    this.renderer = new Renderer(canvas, this.camera, this.scene);
+
+    this.scene.background = new THREE.Color("#1680AF");
+
+    // Camera controls
+    this.controls = new OrbitControls(this.camera, canvas);
+    this.controls.enableDamping = true;
+    this.controls.target.set(25, 0, -10);
+
+    // Lighting
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+    this.scene.add(ambientLight);
+
+    const directLight = new THREE.DirectionalLight();
+    this.scene.add(directLight);
+
+    const axesHelper = new THREE.AxesHelper(50);
+    this.scene.add(axesHelper);
+
+    // Start game
+    this.setupGame();
+    this.update();
+  }
+
+  private setupGame() {
+    // Build the starting lanes
+    const lane = buildLane(this.gameLoader.modelLoader);
+    this.scene.add(lane);
+
+    const lane2 = buildLane(this.gameLoader.modelLoader);
+    lane2.position.z = -20;
+    this.scene.add(lane2);
+
+    // Add the player
+    const box = this.gameLoader.modelLoader.get("box");
+    if (box) {
+      box.position.set(25, 0.01, -2.5);
+      box.scale.set(2, 2, 2);
+      this.scene.add(box);
+      this.player = box;
+    }
+  }
+
+  private playerMovement(dt: number) {
+    if (!this.player) {
+      return;
+    }
+
+    if (this.keyboardListener.isKeyPressed("a")) {
+      this.player.position.x -= this.playerMoveSpeed * dt;
+    } else if (this.keyboardListener.isKeyPressed("d")) {
+      this.player.position.x += this.playerMoveSpeed * dt;
+    }
+    if (this.keyboardListener.isKeyPressed("w")) {
+      this.player.position.z -= this.playerMoveSpeed * dt;
+    } else if (this.keyboardListener.isKeyPressed("s")) {
+      this.player.position.z += this.playerMoveSpeed * dt;
+    }
+  }
+
+  private update = () => {
+    requestAnimationFrame(this.update);
+
+    const dt = this.clock.getDelta();
+
+    this.playerMovement(dt);
+
+    this.renderer.render();
+    this.controls.update();
+  };
+}
---
+++ src/listeners/mouse-listener.ts
@@ -0,0 +1,175 @@
+import { Point } from "../utils/common-types";
+
+export type MouseEventCallback = () => void;
+export type MouseEventType =
+  | "leftclick"
+  | "rightclick"
+  | "leftclickdrag"
+  | "move";
+
+enum MouseButton {
+  LEFT,
+  RIGHT,
+  MIDDLE,
+  NONE,
+  DEFAULT = 0,
+}
+
+interface MouseEventPosition {
+  normalised: Point;
+  canvas: Point;
+  screen: Point;
+  delta: Point;
+}
+
+export class MouseListener {
+  clickPosition: MouseEventPosition = {
+    normalised: { x: 0, y: 0 },
+    canvas: { x: 0, y: 0 },
+    screen: { x: 0, y: 0 },
+    delta: { x: 0, y: 0 },
+  };
+  movePosition: MouseEventPosition = {
+    normalised: { x: 0, y: 0 },
+    canvas: { x: 0, y: 0 },
+    screen: { x: 0, y: 0 },
+    delta: { x: 0, y: 0 },
+  };
+
+  private pointerDownEvent?: PointerEvent;
+  private previousPosition: Point = { x: 0, y: 0 };
+  private readonly clickMovementTolerance = 5;
+
+  private callbacks = new Map<MouseEventType, MouseEventCallback[]>();
+
+  constructor(public canvasElement: HTMLCanvasElement) {
+    this.addEventListeners();
+  }
+
+  on(eventType: MouseEventType, callback: MouseEventCallback) {
+    const existing = this.callbacks.get(eventType) ?? [];
+    existing.push(callback);
+    this.callbacks.set(eventType, existing);
+  }
+
+  off(eventType: MouseEventType, callback: MouseEventCallback) {
+    let existing = this.callbacks.get(eventType) ?? [];
+    if (existing.length) {
+      existing = existing.filter((cb) => cb !== callback);
+      this.callbacks.set(eventType, existing);
+    }
+  }
+
+  private addEventListeners() {
+    this.canvasElement.addEventListener("pointerdown", this.handleMouseDown);
+    this.canvasElement.addEventListener("pointermove", this.handleMouseMove);
+    this.canvasElement.addEventListener("pointerup", this.handleMouseUp);
+  }
+
+  private readonly handleMouseDown = (event: PointerEvent) => {
+    this.pointerDownEvent = event;
+  };
+
+  private readonly handleMouseMove = (event: PointerEvent) => {
+    // Save the position of the cursor
+    this.movePosition = this.getMouseEventPosition(event);
+
+    // If there is a pointer down event, this is a drag
+    if (this.pointerDownEvent) {
+      const button = this.getMouseButtonFrom(event);
+      if (button === MouseButton.LEFT) {
+        this.triggerCallbacks("leftclickdrag");
+      }
+    } else {
+      this.triggerCallbacks("move");
+    }
+  };
+
+  private readonly handleMouseUp = (event: PointerEvent) => {
+    this.clickPosition = this.getMouseEventPosition(event);
+
+    if (!this.pointerDownEvent) {
+      return;
+    }
+
+    if (
+      this.movedLessThan(
+        this.pointerDownEvent,
+        event,
+        this.clickMovementTolerance
+      )
+    ) {
+      // Clicked
+      const button = this.getMouseButtonFrom(event);
+      switch (button) {
+        case MouseButton.LEFT:
+          this.triggerCallbacks("leftclick");
+          break;
+        case MouseButton.RIGHT:
+          this.triggerCallbacks("rightclick");
+          break;
+      }
+    } else {
+      // Drag end
+    }
+
+    this.pointerDownEvent = undefined;
+  };
+
+  private getMouseEventPosition(event: PointerEvent): MouseEventPosition {
+    const rect = this.canvasElement.getBoundingClientRect();
+
+    const normalised: Point = {
+      x: ((event.clientX - rect.left) / this.canvasElement.clientWidth) * 2 - 1,
+      y:
+        -((event.clientY - rect.top) / this.canvasElement.clientHeight) * 2 + 1,
+    };
+
+    const delta: Point = {
+      x: event.clientX - this.previousPosition.x,
+      y: event.clientY - this.previousPosition.y,
+    };
+
+    this.previousPosition = {
+      x: event.clientX,
+      y: event.clientY,
+    };
+
+    const canvas: Point = {
+      x: event.clientX - rect.left,
+      y: event.clientY - rect.top,
+    };
+
+    return {
+      normalised,
+      canvas,
+      screen: this.previousPosition,
+      delta,
+    };
+  }
+
+  private getMouseButtonFrom(event: MouseEvent): MouseButton | undefined {
+    if (event.buttons === 1 || event.button === 0) {
+      return MouseButton.LEFT;
+    } else if (event.buttons === 4 || event.button === 1) {
+      return MouseButton.MIDDLE;
+    } else if (event.buttons === 2 || event.button === 2) {
+      return MouseButton.RIGHT;
+    }
+
+    return undefined;
+  }
+
+  private movedLessThan(a: MouseEvent, b: MouseEvent, radius: number): boolean {
+    return (
+      (a.clientX - b.clientX) * (a.clientX - b.clientX) +
+        (a.clientY - b.clientY) * (a.clientY - b.clientY) <
+      radius * radius
+    );
+  }
+
+  private triggerCallbacks(eventType: MouseEventType) {
+    const callbacks = this.callbacks.get(eventType) ?? [];
+    callbacks.forEach((cb) => cb());
+  }
+}
