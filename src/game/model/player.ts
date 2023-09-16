8d341c3404fefa6137231318df9cf0c0621f9291
MNewbigging <mrnewbigging@gmail.com>
2023-09-16 15:30:59 +0100

    tidy

--- src/game/model/player.ts
+++ src/game/model/player.ts
@@ -1,16 +1,5 @@
 import * as THREE from "three";
 
-/**
- * Player rework:
- * - Remove moveSpeed from player object, put inside player manager instead
- * - Add player effect enum and activeEffects property
- * - Remove canMove, use activeEffects instead
- * -- Need player as a class?
- *
- * Other:
- * - fix: bounds persist between replays, shouldn't (just the helpers not being removed from scene)
- */
-
 export enum PlayerEffect {
   IN_MANHOLE = "in-manhole",
   ON_CROSSING = "on-crossing",
