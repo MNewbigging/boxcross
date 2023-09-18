124edc84506c1efeefdcddebca14d669acdf8497
MNewbigging <mrnewbigging@gmail.com>
2023-09-18 19:29:50 +0100

    night mode test values

--- src/game/lighting-manager.ts
+++ src/game/lighting-manager.ts
@@ -1,6 +1,8 @@
 import * as THREE from "three";
 import { GameStore } from "./game-store";
 
+import GUI from "lil-gui";
+
 export class LightingManager {
   private ambientLight: THREE.AmbientLight;
   private directLight: THREE.DirectionalLight;
@@ -13,17 +15,41 @@ export class LightingManager {
     const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
 
     const directLight = new THREE.DirectionalLight();
-    console.log("intensity", directLight.intensity);
 
     scene.add(ambientLight, directLight);
 
     // Assign refs for later
     this.ambientLight = ambientLight;
     this.directLight = directLight;
+
+    // Testing night mode
+    this.nightMode();
+
+    // Debug GUI
+    const gui = new GUI();
+
+    gui
+      .add(ambientLight, "intensity")
+      .name("ambient intensity")
+      .min(0)
+      .max(1)
+      .step(0.01);
+
+    gui
+      .add(directLight, "intensity")
+      .name("direct intensity")
+      .min(0)
+      .max(1)
+      .step(0.01);
   }
 
   update(dt: number) {
     //
+  }
+
+  private nightMode() {
+    this.ambientLight.intensity = 0.03;
+    this.directLight.intensity = 0.03;
   }
 }
 
